e57a4a93861c23c6baf12c824c7d23f13872d611
louisbookoff <lsbookof@gmail.com>
2018-03-08 12:51:34 -0500

    Retest curl scripts with secure applications

--- app/controllers/trips_controller.rb
+++ app/controllers/trips_controller.rb
@@ -1,4 +1,4 @@
-class TripsController < ApplicationController
+class TripsController < ProtectedController
   before_action :set_trip, only: [:show, :update, :destroy]
 
   # GET /trips
@@ -15,7 +15,8 @@ class TripsController < ApplicationController
 
   # POST /trips
   def create
-    @trip = Trip.new(trip_params)
+    @trip = current_user.trips.build(trip_params)
+    # new(trip_params)
 
     if @trip.save
       render json: @trip, status: :created, location: @trip
@@ -41,7 +42,7 @@ class TripsController < ApplicationController
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_trip
-      @trip = Trip.find(params[:id])
+      @trip = current_user.trips.find(params[:id])
     end
 
     # Only allow a trusted parameter "white list" through.
