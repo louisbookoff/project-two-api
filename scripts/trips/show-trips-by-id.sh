69d53354117af583f415c975781d4ff6066d934f
louisbookoff <lsbookof@gmail.com>
2018-03-08 11:49:31 -0500

    Curl script for delete and show-by-id working properly
    - Curl Scripts for AUTH and TRIPS complete

---
+++ scripts/trips/show-trips-by-id.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# TOKEN="BAhJIiVlMGNjODE1OTg4ZjJmZTEwMTBmZDA5ZWViODg1ZDEwOQY6BkVG--a15807b683ac51e9d927c2195b92584272d82a9f" sh scripts/trips/get-trips.sh
+
+curl "http://localhost:4741/trips/${ID}" \
+  --include \
+  --request GET \
+  --header "Authorization: Token token=${TOKEN}"
+
+echo
